606139ac6a4e23a5de7d1a1a978d7ed6aec6bf73
Florian Dufour <floriandufour66@gmail.com>
2026-03-31 18:15:39 +0200

    Document IElevationSensor
    Added documentation for the IElevationSensor interface and its methods.

--- src/interfaces/IElevationSensor.h
+++ src/interfaces/IElevationSensor.h
@@ -2,8 +2,19 @@
 #include "../core/ElevationData.h"
 #include <chrono>
 
+/**
+ * @brief Elevation sensor interface.
+ */
 class IElevationSensor {
 public:
+     /**
+     * @brief Virtual destructor.
+     */
     virtual ~IElevationSensor() = default;
+    /**
+    * @brief Gets the data from the elevation sensor
+    * @params ts Timestamp of the data collection
+    * @returns ElevationData Collected elevation data
+    */
     virtual ElevationData readElevation(std::chrono::system_clock::time_point ts) = 0;
 };
