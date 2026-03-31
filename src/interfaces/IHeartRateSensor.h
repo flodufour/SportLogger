33b3bf8fc6ee29bdf193bc32548f1141d0fcbfa3
Florian Dufour <floriandufour66@gmail.com>
2026-03-31 18:27:41 +0200

    Doc

--- src/interfaces/IHeartRateSensor.h
+++ src/interfaces/IHeartRateSensor.h
@@ -2,8 +2,19 @@
 #include "../core/HeartRateData.h"
 #include <chrono>
 
+/**
+* @brief Interface for a heart sensor.
+*/
 class IHeartRateSensor {
 public:
+			/**
+			* @brief Virtual destructor.
+			*/
 			virtual ~IHeartRateSensor() = default;
+			/**
+			* @brief Gets heart rate data from the sensor.
+			* @param ts Timestamp of the data collection.
+			* @return HeartRateData Collected heart rate data.
+			*/
 			virtual HeartRateData readHeartRate(std::chrono::system_clock::time_point ts) = 0;
 };
