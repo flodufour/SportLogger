b794a3fed3d8fc37093fe504f23c30d34e0f64c3
Florian Dufour <floriandufour66@gmail.com>
2026-03-31 18:22:00 +0200

    IGPS documentation
    Added documentation for IGPS interface and its methods.

--- src/interfaces/IGPS.h
+++ src/interfaces/IGPS.h
@@ -1,8 +1,20 @@
 #pragma once
 #include "../core/GpsData.h"
 #include <chrono>
+
+/**
+* @brief GPS Sensor interface.
+*/
 class IGPS {
 public:
+    /** 
+    * @brief Virtual destructor.
+    */
     virtual ~IGPS() = default;
+    /** 
+    * @brief Gets GPS data from the sensor.
+    * @param ts TimeStamp of the data collection.
+    * @return GPSData Collected GPS data.
+    */
     virtual GPSData readPosition(std::chrono::system_clock::time_point ts) = 0;
 };
