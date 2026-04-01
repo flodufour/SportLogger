d823c0c297e9ea310170b7617134773b3189bb4c
Florian Dufour <floriandufour66@gmail.com>
2026-04-01 10:46:29 +0200

    doc IUploader.h

--- src/interfaces/IUploader.h
+++ src/interfaces/IUploader.h
@@ -1,10 +1,21 @@
 #pragma once
 #include <string>
 
+/** 
+* @brief Interface for an uploader.
+*/
 class IUploader {
 public:
+    /** 
+    * @brief Virtual destructor.
+    */
     virtual ~IUploader() = default;
-
+    /** 
+    * @brief Uploads an activity.
+    * @param filePath File path of the uploaded file.
+    * @param name Name of the uploaded file.
+    * @param description Description of the uploaded file.
+    */
     virtual bool uploadActivity(const std::string& filePath,
         const std::string& name,
         const std::string& description) = 0;
