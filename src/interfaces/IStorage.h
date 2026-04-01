e849850edb660e67e232cf3324578a70d3a7d4ad
Florian Dufour <floriandufour66@gmail.com>
2026-04-01 10:42:34 +0200

    doc IStorage.h

--- src/interfaces/IStorage.h
+++ src/interfaces/IStorage.h
@@ -1,8 +1,19 @@
 #pragma once
 #include "../core/Activity.h"
 
+/** 
+* @brief Interface for the storage.
+*/
 class IStorage {
 public:
+    /**
+    * @brief Virtual destructor.
+    */
     virtual ~IStorage() = default;
+    /**
+    * @brief Saves the actvity.
+    * @param activity Activity to be Saved
+    * @return void
+    */
     virtual void saveActivity(const Activity& activity) = 0;
 };
