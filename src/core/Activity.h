dc94bf2bb57ca2d489be1f65588e971566e3f368
Florian Dufour <floriandufour66@gmail.com>
2026-04-03 13:11:57 +0200

    doc Activity.h

--- src/core/Activity.h
+++ src/core/Activity.h
@@ -6,17 +6,47 @@
 #include "HeartRateData.h"
 #include "GPSData.h"
 
+/** 
+* @brief Manages the life cycle of an activity. Handles the collection of the data from the sensors
+*/
 class Activity {
 public:
+
+	/** 
+	* @brief Builds a new activity.
+	* @param name Name of the activity.
+	*/
     Activity(const std::string& name);
 
-    void start();  
+	/** 
+	* @brief Starts the activity.
+	*/
+    void start(); 
+
+	/** 
+	* @brief Stops the activity.
+	*/
     void stop();   
 
+	/** 
+	* @brief Adds an ElevationData to the activity.
+	* @param elevation Elevation data.
+	*/
     void addElevation(const ElevationData& elevation);
+
+	/** 
+	* @brief Adds an HeartRateData to the activity.
+	* @param data HeartRateData data.
+	*/
     void addHeartRate(const HeartRateData& data);
+
+	/** 
+	* @brief Adds a GPSData to the activity.
+	* @param data GPSData data.
+	*/
 	void addGPS(const GPSData& data);
 
+	// Getters
     std::string getName() const;
     std::chrono::system_clock::time_point getStartTime() const;
     std::chrono::system_clock::time_point getEndTime() const;
@@ -29,7 +59,7 @@ private:
     std::string name;
     std::chrono::system_clock::time_point startTime;
     std::chrono::system_clock::time_point endTime;
-    bool running = false;
+    bool running = false; // Internal state
     std::vector<ElevationData> elevations;
 	std::vector<HeartRateData> heartRates;
 	std::vector<GPSData> gps;
