453fb0c694bc76c0a94343d663c72f132c37155f
Florian Dufour <floriandufour66@gmail.com>
2026-04-04 19:41:13 +0200

    doc HeartRateData.h

--- src/core/HeartRateData.h
+++ src/core/HeartRateData.h
@@ -1,10 +1,21 @@
 #pragma once
 #include <chrono>
-
+   
+/** 
+* @class HeartRateData
+* @brief Represents heart Rate mesure and their mesure time.
+*/
 class HeartRateData {
 public: 
+	 /**
+    * @brief Constructor of HeartRateData.
+    * @param heartRate HeartRate in bpm.
+    * @param oxygenSaturation OxygenSaturation in SPo2.
+    * @param timestamp Timestamp corresponding to the mesure.
+    */
 	HeartRateData(float heartRate, float oxygenSaturation, std::chrono::system_clock::time_point timestamp);
-	
+
+	// Getters
 	float getOxygenSaturation() const;
 	float getHeartRate() const;
 	std::chrono::system_clock::time_point getTimestamp() const;
