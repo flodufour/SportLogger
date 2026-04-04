4c1bce60d6ff9ffd943b13f64753b2540f110e02
Florian Dufour <floriandufour66@gmail.com>
2026-04-04 15:46:38 +0200

    doc ElevationData.h

--- src/core/ElevationData.h
+++ src/core/ElevationData.h
@@ -1,10 +1,20 @@
 #pragma once
 #include <chrono>
 
+/** 
+* @class Elevationdata
+* @brief Represents an elevation mesure and it's mesure time.
+*/
 class ElevationData {
 public:
+    /**
+     * @brief Constructor of ElevationData.
+     * @param elevationMeters Elevation in meters.
+     * @param timestamp Timestamp corresponding to the mesure.
+     */
     ElevationData(float elevationMeters, std::chrono::system_clock::time_point timestamp);
 
+    // Getters
     float getElevation() const;
     std::chrono::system_clock::time_point getTimestamp() const;
 
