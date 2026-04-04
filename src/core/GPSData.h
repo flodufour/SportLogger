0c9a197582168e49853944f8952062643d4e7d1a
Florian Dufour <floriandufour66@gmail.com>
2026-04-04 15:50:25 +0200

    doc GPSData.h

--- src/core/GPSData.h
+++ src/core/GPSData.h
@@ -1,11 +1,22 @@
 #pragma once
-#include <chrono>
+#include <chrono>    
 
+/** 
+* @class GPSData
+* @brief Represents cordinnates mesure and their mesure time.
+*/
 class GPSData {
 public:
+    /**
+    * @brief Constructor of GPSData.
+    * @param latitude Latitude in degrees.
+    * @param longitude Longitude in degrees.
+    * @param timestamp Timestamp corresponding to the mesure.
+    */
     GPSData(double latitude, double longitude,
         std::chrono::system_clock::time_point timestamp);
 
+    // Getters
     double getLatitude() const;
     double getLongitude() const;
     std::chrono::system_clock::time_point getTimestamp() const;
